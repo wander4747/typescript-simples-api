3e8d20f7d088f1ca534bb08f1a6a6525f5167ec7
Wander <wander.bicalho@feedz.com.br>
2025-04-25 11:52:33 -0300

    config express

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+const express = require('express')
+import dotenv from 'dotenv'
+dotenv.config()
+
+const PORT = process.env.PORT || 8080
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+  console.log(`server running in port  ${PORT}`)
+})
