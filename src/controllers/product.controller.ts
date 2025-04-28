d40e8698db19015c89f0a766da9a17c97c8d3b6f
Wander <wander.bicalho@feedz.com.br>
2025-04-28 07:18:19 -0300

    find all products

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,10 +1,14 @@
 import {Request, response, Response} from 'express'
+import AppDataSource from '../database/connection'
+import { Product } from '../entities/product.entity'
 
 class ProductController {
 
     findAll = async (request: Request, response: Response): Promise<Response> => {
+        const products = await AppDataSource.getRepository(Product).find()
+
         return response.status(200).send({
-          data: []
+            data: products
         })
     }
 }
