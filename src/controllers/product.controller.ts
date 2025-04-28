18b32fc049332ee072e36271faf1c4301493005a
Wander <wander.bicalho@feedz.com.br>
2025-04-28 07:31:25 -0300

    create product

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,14 +1,34 @@
 import {Request, response, Response} from 'express'
 import AppDataSource from '../database/connection'
 import { Product } from '../entities/product.entity'
+import { Repository } from 'typeorm'
 
 class ProductController {
 
+    private repository: Repository<Product>
+
+    constructor() {
+        this.repository = AppDataSource.getRepository(Product)
+    }
+    
     findAll = async (request: Request, response: Response): Promise<Response> => {
-        const products = await AppDataSource.getRepository(Product).find()
+        const products = await this.repository.find()
 
         return response.status(200).send({
             data: products
+        })
+    }
+
+    create = async (request: Request, response: Response): Promise<Response> => {
+        const product = new Product();
+        product.name = "PC"
+        product.description = "PC AMD"
+        product.weight = 5
+
+        const productDb = await this.repository.save(product)
+
+        return response.status(201).send({
+            data: productDb
         })
     }
 }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import productController from "../controllers/product.controller"
 const routes = Router()
 
 routes.get('/api/products', productController.findAll)
+routes.post('/api/products', productController.create)
 
 
 export default routes
