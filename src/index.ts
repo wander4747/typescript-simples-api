4d29fe8d63d9cb733ced1271a7d1c86c40238871
Wander <wander.bicalho@feedz.com.br>
2025-04-28 09:00:07 -0300

    refactor routes

--- src/index.ts
+++ src/index.ts
@@ -16,8 +16,10 @@ const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 app.use(routes)
-
-
+app.use((req, res, next) => {
+    res.status(404).send('Sorry cant find that!')
+})
+  
 app.listen(PORT, () => {
     console.log(`server running in port  ${PORT}`)
 })
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,17 @@
 import { Request, Response, Router } from "express"
-import productController from "@/controllers/product.controller"
+import ProductController from "@/controllers/product.controller"
 
 const routes = Router()
 
-routes.get('/api/products', productController.findAll)
-routes.post('/api/products', productController.create)
-routes.get('/api/products/:id', productController.find)
-routes.put('/api/products/:id', productController.update)
-routes.delete('/api/products/:id', productController.delete)
+routes.get('/api/products', ProductController.findAll)
+routes.post('/api/products', ProductController.create)
+routes.get('/api/products/:id', ProductController.find)
+routes.put('/api/products/:id', ProductController.update)
+routes.delete('/api/products/:id', ProductController.delete)
+routes.get('/', (_: Request, response: Response) => {
+    response.status(200).send({
+        "message": "Hello Dev!"
+    })
+})
 
 export default routes
